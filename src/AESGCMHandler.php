d78484ffdc152e32d989501b5fb1a6260a1fb939
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-26 22:20:41 -0300

    Encryption key validation

--- src/AESGCMHandler.php
+++ src/AESGCMHandler.php
@@ -4,25 +4,25 @@
 
     use STDW\Contract\EncryptionHandlerInterface;
     use STDW\Encryption\AESGCM\Exception\EncryptionCipherNotFoundException;
+    use STDW\Encryption\AESGCM\Exception\EncryptionKeyNotFoundException;
+    use STDW\Support\Str;
 
 
     class AESGCMHandler implements EncryptionHandlerInterface
     {
+        protected string $key;
+
+        protected string $cipher;
+        
         protected $supported_ciphers = [
             'aes-128-gcm',
             'aes-192-gcm',
             'aes-256-gcm',
         ];
 
-        protected string $key;
 
-        protected string $cipher;
-
-
-        public function __construct(string $key, string $cipher = 'aes-128-gcm')
+        public function __construct(string $cipher = 'aes-128-gcm')
         {
-            $this->setKey($key);
-
             $this->cipher = $cipher;
 
             if ( ! in_array($cipher, $this->supported_ciphers)) {
@@ -33,8 +33,16 @@
         }
 
 
+        public function setKey(string $key): void
+        {
+            $this->key = $key;
+        }
+
         public function encrypt(mixed $value): string
         {
+            $this->verifyIfKeyNotEmpty();
+
+
             $ivlen = openssl_cipher_iv_length($this->cipher);
             $iv = openssl_random_pseudo_bytes($ivlen);
             $value = openssl_encrypt($value, $this->cipher, $this->key, 0, $iv, $tag);
@@ -44,6 +52,9 @@
 
         public function decrypt(string $payload): mixed
         {
+            $this->verifyIfKeyNotEmpty();
+
+
             $encrypted = explode('.', $payload);
 
             if (count($encrypted) != 3) {
@@ -60,8 +71,11 @@
             return openssl_decrypt($value, $this->cipher, $this->key, 0, $iv, $tag);
         }
 
-        public function setKey(string $key): void
+
+        protected function verifyIfKeyNotEmpty(): void
         {
-            $this->key = $key;
+            if (Str::empty($this->key)) {
+                throw new EncryptionKeyNotFoundException("Encryption key not found.");
+            }
         }
     }
---
+++ src/Exception/EncryptionKeyNotFoundException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Encryption\AESGCM\Exception;
+
+    use Exception;
+
+
+    class EncryptionKeyNotFoundException extends Exception
+    { }
